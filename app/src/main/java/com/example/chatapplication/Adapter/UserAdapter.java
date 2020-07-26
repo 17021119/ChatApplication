f7f77d9868dacf08c5898cbb842fad0a96a52ea9
TuNguyen <ituet1119@gmail.com>
2020-07-26 11:38:58 +0700

    Start Chat

--- app/src/main/java/com/example/chatapplication/Adapter/UserAdapter.java
+++ app/src/main/java/com/example/chatapplication/Adapter/UserAdapter.java
@@ -1,6 +1,8 @@
 package com.example.chatapplication.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.chatapplication.MessageActivity;
 import com.example.chatapplication.Modal.User;
 import com.example.chatapplication.R;
 
@@ -24,17 +27,20 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
         this.mUsers= mUsers;
         this.mContext= mContext;
     }
-
     @NonNull
     @Override
+//    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View view= LayoutInflater.from(mContext).inflate(R.layout.user_item, parent, false);
+//        return new UserAdapter.ViewHolder(view);
+//    }
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(mContext).inflate(R.layout.user_item, parent, false);
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item, parent, false);
         return new UserAdapter.ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        User user= mUsers.get(position);
+        final User user= mUsers.get(position);
         holder.username.setText(user.getUsername());
         if(user.getImageURL().equals("default")){
             holder.profile_image.setImageResource(R.mipmap.ic_launcher);
@@ -42,6 +48,23 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
         else{
             Glide.with(mContext).load(user.getImageURL()).into(holder.profile_image);
         }
+
+//        holder.itemView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(mContext, MessageActivity.class);
+//                intent.putExtra("userid", user.getId());
+//                mContext.startActivity(intent);
+//            }
+//        });
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, MessageActivity.class);
+                intent.putExtra("userid", user.getId());
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -59,6 +82,8 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
 
             username = itemView.findViewById(R.id.username);
             profile_image= itemView.findViewById(R.id.profile_image);
+
+
         }
     }
 }
